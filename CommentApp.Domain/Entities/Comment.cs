6edcb332875f96490615d558b96b24592b80abd2
Petro Smoliar <143265445+Petro-Smoliar@users.noreply.github.com>
2024-12-12 15:33:56 +0100

    Add User and Comment models with reply support

---
+++ CommentApp.Domain/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace CommentApp.Domain.Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; } = DateTime.Now;
+    
+    public int UserId { get; set; }
+    public User? User { get; set; }
+    
+    public int? ParentCommentId { get; set; }
+    public Comment? ParentComment { get; set; }
+    
+    public ICollection<Comment> Replies { get; set; } = new List<Comment>();
+}
